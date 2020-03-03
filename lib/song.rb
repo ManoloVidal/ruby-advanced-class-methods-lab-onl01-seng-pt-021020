f44aad8114bd1b656afabaebbbadf47be4e562e6
Juan Vidal-Ramos <vidalramosjm@gmail.com>
2020-03-03 20:06:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,12 +50,12 @@ class Song
     
   end 
   
-  #def self.create from_filename
-    #info = filename.split(" - ")
-    #song = self.create 
-    #song.name = info[1].chomp(".mp3")
-    #song.artist_name = info[0]
-    #song 
+  def self.create from_filename(filename)
+    info = filename.split(" - ")
+    song = self.create 
+    song.name = info[1].chomp(".mp3")
+    song.artist_name = info[0]
+    song 
     
  # end 
     
