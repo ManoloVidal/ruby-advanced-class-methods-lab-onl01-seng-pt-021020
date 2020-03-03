c53e378be7aa8b8c438dc1e7971c0e6fe5342a26
Juan Vidal-Ramos <vidalramosjm@gmail.com>
2020-03-03 18:00:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,14 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
   
+  def self.all
+    @@all
+  end
+
+  def save
+    self.class.all << self
+  end
+  
   def self.create
     song = self.new 
 end 
